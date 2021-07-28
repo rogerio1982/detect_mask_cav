0fe2600e19a779062608a2535f63088c0577632a
Rogério Soares <rogerio_si@yahoo.com.br>
2021-07-27 21:10:36 -0300

    Updare

--- sketch.js
+++ sketch.js
@@ -29,7 +29,7 @@ function setup() {
   createCanvas(320, 260);
   // Create the video
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  //video.size(320, 240);
   video.hide();
 
   flippedVideo = ml5.flipImage(video)
